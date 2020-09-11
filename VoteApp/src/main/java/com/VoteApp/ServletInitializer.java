15db48edb1a4019bd5dd7f908f045c1c0f79e08c
ravik9158 <ravik9158@hotmail.com>
2020-09-11 15:04:05 +0530

    initial check-in commit of VoteApp

---
+++ VoteApp/src/main/java/com/VoteApp/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.VoteApp;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(VoteAppApplication.class);
+	}
+
+}
