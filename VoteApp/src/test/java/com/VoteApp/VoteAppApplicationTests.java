b2d7c9b4281170589ae7a250367dc391c4413d57
ravi <ravik9158@hotmail.com>
2021-03-18 13:18:42 +0530

    Initial source code for VoteApp

---
+++ VoteApp/src/test/java/com/VoteApp/VoteAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.VoteApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VoteAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
